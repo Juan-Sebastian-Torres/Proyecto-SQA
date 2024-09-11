fe3956c5dbb069189275f10b93665dc7a0eea7f7
Juan-Sebastian-Torres <sebastiantorres796@gmail.com>
2024-09-11 14:58:44 -0500

    update variable

--- Reto_Tecnico/src/test/java/co/com/screenplay/project/stepdefinition/OpenWebStep.java
+++ Reto_Tecnico/src/test/java/co/com/screenplay/project/stepdefinition/OpenWebStep.java
@@ -7,15 +7,10 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import net.serenitybdd.screenplay.questions.page.TheWebPage;
-import org.hamcrest.Matchers;
 
 import static co.com.screenplay.project.utils.Constants.ACTOR;
-import static co.com.screenplay.project.utils.Constants.TITLE;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class OpenWebStep {
 
--- Reto_Tecnico/src/test/java/co/com/screenplay/project/stepdefinition/hook/Hook.java
+++ Reto_Tecnico/src/test/java/co/com/screenplay/project/stepdefinition/hook/Hook.java
@@ -3,16 +3,8 @@ package co.com.screenplay.project.stepdefinition.hook;
 import co.com.screenplay.project.hook.OpenWeb;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
-import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
-import net.serenitybdd.screenplay.questions.page.TheWebPage;
-import org.hamcrest.Matchers;
-
-import static co.com.screenplay.project.utils.Constants.TIME_SHORT;
-import static co.com.screenplay.project.utils.Constants.TITLE;
-import static co.com.screenplay.project.utils.Time.waitting;
-import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class Hook {
 
@@ -26,12 +18,7 @@ public class Hook {
         OnStage.theActorCalled(actor).attemptsTo(
                 OpenWeb.broserURL()
         );
-        waitting(TIME_SHORT);
-        theActorInTheSpotlight().should(
-                GivenWhenThen.seeThat(
-                        TheWebPage.title(), Matchers.containsString(TITLE)
-                )
-        );
+
     }
 
 }
